ef3930ed2d6de85f65f2f3bf353dad8f98b11e12
Abigail Read <AbbyReadMail@gmail.com>
2025-09-21 13:58:15 -0700

    Fix infinite recursion problem (but not efficiency discrepancies yet)

--- src/main/java/net/fabricmc/abbyread/mixin/ToolItemMixin.java
+++ src/main/java/net/fabricmc/abbyread/mixin/ToolItemMixin.java
@@ -1,6 +1,5 @@
 package net.fabricmc.abbyread.mixin;
 
-import btw.block.blocks.*;
 import btw.community.abbyread.EfficiencyHelper;
 import btw.community.abbyread.UniformEfficiencyModifier;
 import btw.item.BTWItems;
@@ -21,103 +20,38 @@ public abstract class ToolItemMixin {
     @Unique
     float effMod = UniformEfficiencyModifier.VALUE;
 
-    @Inject(method = "onBlockDestroyed", at = @At("HEAD"), cancellable = true)
+    @Unique
+    protected float effOnProp = 4F;
+
+    // Because neither ChiselItem nor ChiselItemWood override this method
+    @Inject(method = "getStrVsBlock", at = @At("HEAD"), cancellable = true, require = 1)
+    private void abbyread$getStrVsBlock(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
+        // Mimic ToolItem's getStrVsBlock method but with special treatment
+        if (    stack != null && (
+                stack.getItem().itemID == BTWItems.pointyStick.itemID ||
+                stack.getItem().itemID == BTWItems.sharpStone.itemID)) {
+            if (world != null && EfficiencyHelper.isToolItemEfficientVsBlock(stack, world, block, x, y, z))
+            {
+                cir.setReturnValue(effOnProp);
+            }
+            cir.setReturnValue(1F);
+        }
+    }
+
+    // DURABILITY
+    @Inject(method = "onBlockDestroyed", at = @At("RETURN"), cancellable = true)
     private void abbyread$preventWastedUses(ItemStack stack, World world, int blockID, int x, int y, int z,
-                                        EntityLivingBase entity, CallbackInfoReturnable<Boolean> cir) {
-        // Normal durability handling
-        boolean effective = false;
+                                            EntityLivingBase entity, CallbackInfoReturnable<Boolean> cir) {
+        boolean effective;
         Block block = Block.blocksList[blockID];
-        if (stack.getItem() instanceof ChiselItemWood ||
-            stack.getItem() instanceof ChiselItemStone) {
+        if (world != null &&
+                stack.getItem() instanceof ChiselItemWood ||
+                stack.getItem() instanceof ChiselItemStone) {
             effective = EfficiencyHelper.isToolItemEfficientVsBlock(stack, world, block, x, y, z);
-        } else  {
+        } else {
             effective = stack.getItem().isEfficientVsBlock(stack, world, block, x, y, z);
         }
         if (effective) stack.damageItem(1, entity);
         cir.setReturnValue(true); // always return true to cancel vanilla handling
-    }
-
-    // Because neither ChiselItem nor ChiselItemWood override this method
-    @Inject(method = "getStrVsBlock", at = @At("RETURN"), cancellable = true)
-    private void abbyread$pointyStickEffModBoost(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
-        if (stack.getItem() instanceof ChiselItemWood) {
-            final int GRASS_SPARSE = 1;
-            final int DIRTSLAB_DIRT = 0;
-            final int PACKED_EARTH = 6;
-            float efficiency = cir.getReturnValue() * effMod;
-            if (block instanceof DirtSlabBlock && world.getBlockMetadata(x, y, z) == DIRTSLAB_DIRT)
-                cir.setReturnValue(efficiency);
-            if (block instanceof BlockGrass && world.getBlockMetadata(x, y, z) == GRASS_SPARSE)
-                cir.setReturnValue(efficiency);
-            if (block instanceof GrassSlabBlock && ((GrassSlabBlock) block).isSparse(world, x, y, z))
-                cir.setReturnValue(efficiency);
-            if (block instanceof AestheticOpaqueEarthBlock && world.getBlockMetadata(x, y, z) == PACKED_EARTH)
-                cir.setReturnValue(efficiency);
-            if (block instanceof BlockDirt)
-                cir.setReturnValue(efficiency);
-        }
-    }
-
-    // Because ChiselItemStone defers to the superclass definition
-    //   (this one) before boosting web harvest speed.
-    @Inject(
-            method = "getStrVsBlock",
-            at = @At("RETURN"),
-            cancellable = true
-    )
-    public void abbyread$sharpStoneEffModBoost(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
-        if (block instanceof BlockGrass grass) {;
-            float efficiency = cir.getReturnValue();
-            if (!grass.isSparse(world, x, y, z)) {
-                System.out.println("full grass");
-                System.out.println("cir.getReturnValue(): " + cir.getReturnValue());
-                System.out.println("efficiency * " + effMod + ": " + efficiency * effMod);
-                cir.setReturnValue(efficiency * effMod);
-            }
-            if (grass.isSparse(world, x, y, z)) {
-                System.out.println("sparse grass");
-                System.out.println("cir.getReturnValue(): " + cir.getReturnValue());
-                System.out.println("efficiency: " + efficiency);
-                cir.setReturnValue(efficiency);
-            }
-        }
-        if (
-                block instanceof BlockGlass ||
-                block instanceof BlockPane ||
-                block instanceof BlockGlowStone ||
-                block instanceof BlockIce ||
-                block instanceof BlockRedstoneLight ||
-                block instanceof BlockStone ||
-                (block instanceof RoughStoneBlock &&
-                        ((RoughStoneBlock) block).strataLevel == 0)
-        ) {
-            float efficiency = cir.getReturnValue() * effMod;
-            cir.setReturnValue(efficiency);
-        }
-    }
-
-    // Put last as a catch-all for things that aren't efficient
-    @Inject(method = "getStrVsBlock", at = @At("RETURN"), cancellable = true)
-    private void abbyread$baselineEffModBoost(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
-        boolean effectiveOn = tool.isEfficientVsBlock(stack, world, block, x, y, z);
-        if (!effectiveOn) {
-            float efficiency = cir.getReturnValue();
-            if (
-                    block instanceof BlockSand // small universal boost
-            ) {
-                efficiency *= effMod;
-                cir.setReturnValue(efficiency);
-            } else if (
-                    block instanceof LooseDirtBlock ||
-                            block instanceof LooseSparseGrassBlock ||
-                            block instanceof LooseSparseGrassSlabBlock ||
-                            block instanceof LooseDirtSlabBlock ||
-                            block instanceof ChewedLogBlock ||
-                            block instanceof BlockLog
-            ) {
-                efficiency *= effMod * 1.5F;
-                cir.setReturnValue(efficiency);
-            }
-        }
     }
 }
