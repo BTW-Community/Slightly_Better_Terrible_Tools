daf21ddf32446a137e1ca062d9a200c0fe6a76a6
Abigail Read <AbbyReadMail@gmail.com>
2025-09-21 22:50:55 -0700

    Remove unnecessary mixin

--- src/main/java/net/fabricmc/abbyread/mixin/DirtSlabBlockMixin.java
+++
@@ -1,9 +0,0 @@
-package net.fabricmc.abbyread.mixin;
-
-import btw.block.blocks.DirtSlabBlock;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(DirtSlabBlock.class)
-public class DirtSlabBlockMixin {
-
-}
