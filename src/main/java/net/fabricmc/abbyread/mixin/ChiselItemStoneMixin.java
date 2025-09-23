9c1fdbd414f8521ec13ab12b471393d1866189b1
Abigail Read <AbbyReadMail@gmail.com>
2025-09-23 12:18:54 -0700

    Revert effMod speed increase for sharp stone (too powerful; special boosts only now)

--- src/main/java/net/fabricmc/abbyread/mixin/ChiselItemStoneMixin.java
+++ src/main/java/net/fabricmc/abbyread/mixin/ChiselItemStoneMixin.java
@@ -8,12 +8,12 @@ import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChiselItemStone.class)
 public class ChiselItemStoneMixin {
 
+    /*
     // Follow-up BTW's efficiencyOnProperMaterial /= 2 with a *= effMod
     @Inject(
             method = "applyStandardEfficiencyModifiers",
@@ -26,6 +26,7 @@ public class ChiselItemStoneMixin {
         final float effMod = EfficiencyHelper.effMod;
         accessor.setEfficiencyOnProperMaterial(original * effMod);
     }
+    */
 
     @Inject(method = "getStrVsBlock", at = @At("HEAD"), cancellable = true)
     private void abbyread$getStrVsBlock(ItemStack stack, World world, Block block,
