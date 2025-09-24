1ab8c62cdc345658589fd17404ce9ef19fdeca6f
Abigail Read <AbbyReadMail@gmail.com>
2025-09-23 21:04:53 -0700

    Cover all bases regarding damage calc based on efficiency

--- src/main/java/net/fabricmc/abbyread/mixin/BattleAxeItemMixin.java
+++ src/main/java/net/fabricmc/abbyread/mixin/BattleAxeItemMixin.java
@@ -1,7 +1,7 @@
 package net.fabricmc.abbyread.mixin;
 
 import btw.community.abbyread.EfficiencyHelper;
-import btw.item.items.AxeItem;
+import btw.item.items.BattleAxeItem;
 import net.minecraft.src.Block;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
@@ -10,13 +10,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AxeItem.class)
-public class AxeItemMixin {
+@Mixin(BattleAxeItem.class)
+public class BattleAxeItemMixin {
 
-    @Inject(method = "isToolTypeEfficientVsBlockType", at = @At("RETURN"), remap = false)
-    private void abbyread$tellHelper(Block block, CallbackInfoReturnable<Boolean> cir) {
-        EfficiencyHelper.setLastEffective(cir.getReturnValue());
-    }
     @Inject(method = "canHarvestBlock", at = @At("RETURN"), remap = false)
     private void abbyread$tellHelper2(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
         EfficiencyHelper.setLastEffective(cir.getReturnValue());
--- src/main/java/net/fabricmc/abbyread/mixin/HoeItemMixin.java
+++ src/main/java/net/fabricmc/abbyread/mixin/HoeItemMixin.java
@@ -3,6 +3,8 @@ package net.fabricmc.abbyread.mixin;
 import btw.community.abbyread.EfficiencyHelper;
 import btw.item.items.HoeItem;
 import net.minecraft.src.Block;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HoeItem.class)
 public class HoeItemMixin {
-    @Inject(method = "isToolTypeEfficientVsBlockType", at = @At("HEAD"), remap = false)
-    private void abbyread$additionalEfficiencyChecks(Block block, CallbackInfoReturnable<Boolean> cir) {
-        if (block.areHoesEffectiveOn()) EfficiencyHelper.setLastEffective(true);
+
+    @Inject(method = "isToolTypeEfficientVsBlockType", at = @At("RETURN"), remap = false)
+    private void abbyread$tellHelper(Block block, CallbackInfoReturnable<Boolean> cir) {
+        EfficiencyHelper.setLastEffective(cir.getReturnValue());
+    }
+    @Inject(method = "canHarvestBlock", at = @At("RETURN"), remap = false)
+    private void abbyread$tellHelper2(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        EfficiencyHelper.setLastEffective(cir.getReturnValue());
     }
 }
