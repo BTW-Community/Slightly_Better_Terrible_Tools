434543f04e4b616413cc11223f0d2016a3cda57d
Abigail Read <AbbyReadMail@gmail.com>
2025-10-21 01:26:58 -0700

    Change to inject at return in order to avoid overriding

--- src/main/java/btw/community/abbyread/sbtt/mixin/PlayerControllerMPMixin.java
+++ src/main/java/btw/community/abbyread/sbtt/mixin/PlayerControllerMPMixin.java
@@ -16,11 +16,10 @@ public class PlayerControllerMPMixin {
     private boolean checking = false;
 
     @Inject(method = "onPlayerRightClick",
-            at = @At("HEAD"),
-            cancellable = true)
-    public void onPlayerRightClickIntercept(EntityPlayer player, World world, ItemStack stack,
-                                            int x, int y, int z, int side, Vec3 hitVec,
-                                            CallbackInfoReturnable<Boolean> cir) {
+            at = @At("RETURN"))
+    public void onPlayerRightClickDamage(EntityPlayer player, World world, ItemStack stack,
+                                         int x, int y, int z, int side, Vec3 hitVec,
+                                         CallbackInfoReturnable<Boolean> cir) {
         if (checking || world == null || player == null || stack == null) {
             return;
         }
@@ -30,7 +29,7 @@ public class PlayerControllerMPMixin {
             Block block = Block.blocksList[world.getBlockId(x, y, z)];
             if (block == null) return;
 
-            // Transfer state from Convert globals (see if block converted via that helper)
+            // Transfer state from Convert globals
             boolean conversionByTool = Convert.justConverted;
             Convert.justConverted = false;
             int itemDamageAmount = Convert.itemDamageAmount;
@@ -39,18 +38,15 @@ public class PlayerControllerMPMixin {
             // Checks if this item + block combo counts as "useful"
             boolean specialCase = ItemUseRegistry.usefulRightClickCombo(stack, block, world.getBlockMetadata(x, y, z));
 
+            // Only damage if conversion or special case triggered
             boolean shouldDamage = conversionByTool || specialCase;
 
             if (shouldDamage) {
-                // Confusing, but item-side logic for handling consequences of block interaction
                 boolean didAffectTool = stack.getItem().onBlockDestroyed(stack, world, block.blockID, x, y, z, player);
                 if (didAffectTool) {
                     player.addStat(StatList.objectUseStats[stack.itemID], itemDamageAmount);
                 }
             }
-
-            // Cancel original so we control damage fully
-            cir.setReturnValue(true);
 
         } finally {
             checking = false;
