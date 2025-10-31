4f4a76e8781f7226139fcf7f139f5c09f13036eb
Abigail Read <AbbyReadMail@gmail.com>
2025-10-31 01:26:18 -0700

    Rename things and improve comments

--- src/main/java/btw/community/abbyread/sbtt/mixin/speed/RoughStoneBlock_BlockMixin.java
+++ src/main/java/btw/community/abbyread/sbtt/mixin/speed/RoughStoneBlock_BlockMixin.java
@@ -14,11 +14,13 @@ public class RoughStoneBlock_BlockMixin {
     // Changes hardness of last bits of rough stone block harvest since chisels
     //  were made effective on that part too.  Still gotta be slower to disincentivize mining mostly.
     @Inject(method = "getBlockHardness", at = @At("RETURN"), cancellable = true)
-    public void abbyread$getBlockHardness(World world, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
+    public void roughStoneIsNotAsSlowToDestroy(World world, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
         Block block = (Block)(Object)this;
         if (!(block instanceof RoughStoneBlock)) return;
 
         float hardness = cir.getReturnValue();
+
+        // Working from the assumption that there's a boost already from chisels being effective-on
         if (world.getBlockMetadata(x, y, z) >= 8) cir.setReturnValue(hardness * 2);
     }
 }
