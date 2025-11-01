bd8b8b1fb089304743fe43a9ef28f43101096624
Abigail Read <AbbyReadMail@gmail.com>
2025-11-01 15:55:25 -0700

    Nerf sparsening grass speed overall a bit

--- src/main/java/btw/community/abbyread/sbtt/mixin/speed/ChiselItemStoneMixin.java
+++ src/main/java/btw/community/abbyread/sbtt/mixin/speed/ChiselItemStoneMixin.java
@@ -22,13 +22,25 @@ public class ChiselItemStoneMixin {
         int meta = world.getBlockMetadata(i, j, k);
         float mod = 1.5F;
 
+        // Nerf efficiency boost from marking can-convert on grass, making it come up
+        //   as efficient on, causing a too-high speed boost.
+        if (ThisBlock.is(BlockType.GRASS, block, meta)) {
+            float base = cir.getReturnValue();
+            cir.setReturnValue(base * 0.75F);
+            System.out.println("cir.getReturnValue()" + cir.getReturnValue());
+            // Fall-through to also nerf loose sparse grass if it is that
+        }
         // Nerf loose sparse grass sparsening speed, as the speed-up from loose dirtlike
         //   is only supposed to be for breaking the block.
         if (ThisBlock.isAll(block, meta, BlockType.LOOSE_DIRTLIKE, BlockType.SPARSE, BlockType.GRASS)) {
             float base = cir.getReturnValue();
+            System.out.println("cir.getReturnValue()" + cir.getReturnValue());
             // Since universal speed-up on loose dirtlike is base * (Globals.modifier * 1.5F),
             //   undo that by dividing by it.
             cir.setReturnValue(base / (Globals.modifier * 1.5F));
+            // Still gets worked faster somehow, but not terribly much.  Feels
+            //   right for it taking an extra step to loosen if not already by pig, etc.
+            return;
         }
 
         // ** Boost categories common to chisels stone and above **
@@ -40,6 +52,7 @@ public class ChiselItemStoneMixin {
             float base = cir.getReturnValue();
             float modifier = (Globals.modifier - 1) * mod + 1;
             cir.setReturnValue(base * modifier);
+            return;
         }
 
         // Solid, single-harvest stonelike blocks easier to pick up with chisels
@@ -47,6 +60,7 @@ public class ChiselItemStoneMixin {
             float base = cir.getReturnValue();
             float modifier = (Globals.modifier - 1) * mod + 1;
             cir.setReturnValue(base * modifier);
+            return;
         }
 
         // Shatterables shattered or picked up faster by chisels
@@ -54,6 +68,7 @@ public class ChiselItemStoneMixin {
             float base = cir.getReturnValue();
             float modifier = (Globals.modifier - 1) * mod + 1;
             cir.setReturnValue(base * modifier);
+            return;
         }
     }
 }
