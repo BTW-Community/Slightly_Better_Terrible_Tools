4b3cba78f2ddfc8f9d6e7140b5d8ff5c191ed9f5
Abigail Read <AbbyReadMail@gmail.com>
2025-11-01 02:08:32 -0700

    Rename and return early

--- src/main/java/btw/community/abbyread/sbtt/mixin/speed/BlockMixin.java
+++ src/main/java/btw/community/abbyread/sbtt/mixin/speed/BlockMixin.java
@@ -19,29 +19,35 @@ public class BlockMixin {
             at = @At("RETURN"),
             cancellable = true
     )
-    private void abbyread$getPlayerRelativeBlockHardness(EntityPlayer player, World world, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
+    private void boostSpeedRegardlessOfTool(EntityPlayer player, World world, int x, int y, int z, CallbackInfoReturnable<Float> cir) {
         Block self = (Block)(Object)this;
         int meta = world.getBlockMetadata(x, y, z);
 
         Set<BlockType> tags = ThisBlock.of(self, meta);
+
+        // Regardless of tool (including nothing), boost by global modifier
         if (tags.contains(BlockType.LOG)) {
             float base = cir.getReturnValue();
             cir.setReturnValue(base * Globals.modifier);
+            return;
         }
 
         if (tags.contains(BlockType.LOOSE_DIRTLIKE)) {
             float base = cir.getReturnValue();
             cir.setReturnValue(base * Globals.modifier * 1.5F);
+            return;
         }
 
         if (tags.contains(BlockType.SAND)) {
             float base = cir.getReturnValue();
             cir.setReturnValue(base * Globals.modifier * 1.25F);
+            return;
         }
 
         if (tags.contains(BlockType.GRAVEL)) {
             float base = cir.getReturnValue();
             cir.setReturnValue(base * Globals.modifier);
+            return;
         }
 
     }
